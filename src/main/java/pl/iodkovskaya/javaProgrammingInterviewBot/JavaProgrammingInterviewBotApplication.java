43808b9eb1a883fa7abf6bd1f23318e3d3de5de5
Alena Iadkouskaya <eiodkovskaya6@gmail.com>
2025-02-26 16:21:28 +0100

    Initial commit

---
+++ src/main/java/pl/iodkovskaya/javaProgrammingInterviewBot/JavaProgrammingInterviewBotApplication.java
@@ -0,0 +1,13 @@
+package pl.iodkovskaya.javaProgrammingInterviewBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProgrammingInterviewBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProgrammingInterviewBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/iodkovskaya/javaProgrammingInterviewBot/JavaProgrammingInterviewBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.iodkovskaya.javaProgrammingInterviewBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProgrammingInterviewBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
