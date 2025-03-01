8d763a1a2d61654c9f7f5725ff5cf08280632a43
Alena Iadkouskaya <eiodkovskaya6@gmail.com>
2025-03-01 17:12:42 +0100

    Implementation of the interview start. Sending a command to start, making a request to ChatGPT, receiving a response from it in Telegram.

---
+++ src/main/java/pl/iodkovskaya/javaProgrammingInterviewBot/dto/GptResponse.java
@@ -0,0 +1,20 @@
+package pl.iodkovskaya.javaProgrammingInterviewBot.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class GptResponse {
+    private List<Choice> choices;
+
+    @Data
+    public static class Choice {
+        private Message message;
+    }
+
+    @Data
+    public static class Message {
+        private String content;
+    }
+}
